ec09fda232131dad7327409cbb4dbdaebaebadde
Matt Gaidica <matt@gaidi.ca>
2020-06-04 09:24:24 -0400

    use char vector

--- util/sunriseSunset.m
+++ util/sunriseSunset.m
@@ -2,7 +2,7 @@ function [sunrise,sunset] = sunriseSunset(t_datetime)
 if ismac
     ssPath = '/Users/matt/Documents/Data/KRSP/SunriseSunset';
 else
-    ssPath = "C:\Users\mgaidica\Documents\Data\KRSP\SunriseSunset";
+    ssPath = 'C:\Users\mgaidica\Documents\Data\KRSP\SunriseSunset';
 end
 T = readtable(fullfile(ssPath,['ss_',num2str(year(t_datetime)),'.txt']));
 [v,k] = min(abs(T.sunrise - t_datetime));
