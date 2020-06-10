cde0968418caa6ec20aa15dd8e0eded0e3867997
Matt Gaidica <matt@gaidi.ca>
2020-06-10 10:39:31 -0400

    sending to recompile

--- util/compile_odba.m
+++ util/compile_odba.m
@@ -4,26 +4,28 @@ decimateBy = 100;
 files = dir2(filespath,'*.csv','-r');
 for iFile = 1:numel(files)
     readFile = fullfile(filespath,files(iFile).name);
-    saveFile = [readFile,'.mat'];
-    if ~exist(saveFile)
-        disp(['working on ',readFile]);
-        inputTable = readtable(readFile);
-        nRange = 1:decimateBy:size(inputTable,1);
-        T = table;
-        if ismember('datetime',inputTable.Properties.VariableNames)
-            dtData = inputTable.datetime(nRange);
-        else
-            dtData = inputTable.dtime(nRange);
-        end
-        if isa(dtData(1),'datetime')
-            T.datetime = dtData;
-        else
-            T.datetime = datetime(dtData,'InputFormat','dd-MM-yyyy HH:mm:ss.00000');
-        end
-        T.odba = inputTable.odba(nRange);
-        T.temp = inputTable.tempC(nRange);
-        save(saveFile,'T');
+    disp(['working on ',readFile]);
+    inputTable = readtable(readFile);
+    nRange = 1:decimateBy:size(inputTable,1);
+    T = table;
+    if ismember('datetime',inputTable.Properties.VariableNames)
+        dtData = inputTable.datetime(nRange);
     else
-        disp([readFile,' already exists, skipping']);
+        dtData = inputTable.dtime(nRange);
     end
+    if days(dtData(end)-dtData(1)) > 90 % assume longest deploy
+        disp('...date converted');
+        dtData = datetime(year(dtData),day(dtData),month(dtData),...
+            hour(dtData),minute(dtData),second(dtData));
+    end
+    if isa(dtData(1),'datetime')
+        T.datetime = dtData;
+    else
+        T.datetime = datetime(dtData,'InputFormat','dd-mmm-yyyy HH:MM:SS');
+    end
+    T.odba = inputTable.odba(nRange);
+    T.temp = inputTable.tempC(nRange);
+    % ADD FIRST DATETIME to filename
+    saveFile = [readFile,'_',datestr(dtData(1),'yyyymmdd'),'.mat'];
+    save(saveFile,'T');
 end
