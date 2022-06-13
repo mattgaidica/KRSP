2a6cbf63fb0230e76c4c0eb31d2136bb8a301d48
Matt Gaidica <matt@gaidi.ca>
2022-06-13 10:02:31 -0400

    Update convertRTable.m

--- R/convertRTable.m
+++ R/convertRTable.m
@@ -41,6 +41,44 @@ VariableNames = {'Comparison','Estimate','SE','df','t','p'};
 data = cell(size(T,1),numel(VariableNames));
 Tnew = cell2table(data,'VariableNames',VariableNames);
 
+rowCount = 0;
+for iRow = 1:size(T,1)
+    if T{iRow,1} == T{iRow,4} && T{iRow,2} ~= T{iRow,5}
+        rowCount = rowCount + 1;
+        Tnew{rowCount,1} = {sprintf('%s %s - %s %s',seasonLabels{T{iRow,1}},mastLabels{T{iRow,2}+1},seasonLabels{T{iRow,4}},mastLabels{T{iRow,5}+1})};
+        Tnew{rowCount,2} = {T{iRow,6}};
+        Tnew{rowCount,3} = {T{iRow,7}};
+        Tnew{rowCount,4} = {T{iRow,8}};
+        Tnew{rowCount,5} = {T{iRow,9}};
+        Tnew{rowCount,6} = {T{iRow,10}};
+    end
+end
+writetable(Tnew,[filename,'.xlsx']);
+
+%%
+filename = '/Users/matt/Dropbox (Personal)/Science/KRSP Axy Manuscript/QBday_pairwise_season-mast.csv';
+T = readtable(filename);
+VariableNames = {'Comparison','Estimate','SE','df','t','p'};
+data = cell(size(T,1),numel(VariableNames));
+Tnew = cell2table(data,'VariableNames',VariableNames);
+
+for iRow = 1:size(T,1)
+    Tnew{iRow,1} = {sprintf('%s %s - %s %s',seasonLabels{T{iRow,1}},mastLabels{T{iRow,2}+1},seasonLabels{T{iRow,4}},mastLabels{T{iRow,5}+1})};
+    Tnew{iRow,2} = {T{iRow,6}};
+    Tnew{iRow,3} = {T{iRow,7}};
+    Tnew{iRow,4} = {T{iRow,8}};
+    Tnew{iRow,5} = {T{iRow,9}};
+    Tnew{iRow,6} = {T{iRow,10}};
+end
+writetable(Tnew,[filename,'.xlsx']);
+
+%%
+filename = '/Users/matt/Dropbox (Personal)/Science/KRSP Axy Manuscript/QBnight_pairwise_season-mast.csv';
+T = readtable(filename);
+VariableNames = {'Comparison','Estimate','SE','df','t','p'};
+data = cell(size(T,1),numel(VariableNames));
+Tnew = cell2table(data,'VariableNames',VariableNames);
+
 for iRow = 1:size(T,1)
     Tnew{iRow,1} = {sprintf('%s %s - %s %s',seasonLabels{T{iRow,1}},mastLabels{T{iRow,2}+1},seasonLabels{T{iRow,4}},mastLabels{T{iRow,5}+1})};
     Tnew{iRow,2} = {T{iRow,6}};
