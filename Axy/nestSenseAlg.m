a82f1c10e3a23fd8fa834a0ff667c12e0a6c8c53
Matt Gaidica <matt@gaidi.ca>
2022-12-30 15:41:18 -0500

    comments

--- Axy/nestSenseAlg.m
+++ Axy/nestSenseAlg.m
@@ -42,9 +42,12 @@ if senseParams.fixTransitions
     doPlot = false;
     maxShift = 60*5; % window
     transLocs = [1;find(abs(diff(binNestSense))==1);numel(binNestSense)]; % pad to rm logic below
-    
+    % the norm pdf weighs location nearest to center as more important,
+    % could also consider using relative heights?
     weightDist = normalize(normpdf(linspace(-1,1,maxShift*2)),'range',[0 1]);
     for ii = 2:numel(transLocs)-1 % exclude pads
+        % the following is to make sure numel(useRange) is maintained but
+        % data from non-existant ids OR nearby trasitions is ignored
         useRange = transLocs(ii)-maxShift+1:transLocs(ii)+maxShift;
         usefulStart = max([1,transLocs(ii-1)]);
         usefulEnd = min([numel(binNestSense),transLocs(ii+1)]);
@@ -53,18 +56,18 @@ if senseParams.fixTransitions
         if isempty(fillStart); fillStart = 1; end
         if isempty(fillEnd); fillEnd = numel(useRange); end
         if fillEnd-fillStart < 120; continue; end
-        
+        % data used for sensing
         nanData = NaN(size(useRange));
         use_tempZ = nanData;
         use_tempZ(fillStart:fillEnd) = sense.tempZ(useRange(fillStart):useRange(fillEnd));
-        
+        % use gradient instead of diff to maintain dimensions
         sensorData1 = normalize(weightDist .* smoothdata(gradient(use_tempZ),'gaussian',60));
         if binNestSense(transLocs(ii)) == 1 % in to out, search for neg peak
             sensorData1 = -sensorData1;
         end
         sensorData2 = normalize(gradient(sensorData1));
-        [~,k] = max(sensorData2);
-        
+        [~,k] = max(sensorData2); % find peak
+        % fill data
         newData = binNestSense(useRange);
         if k >= maxShift
             newData(maxShift:k) = binNestSense(transLocs(ii));
@@ -91,6 +94,6 @@ if senseParams.fixTransitions
             grid on;
             hold off;
         end
-        binNestSense(useRange) = newData;
+        binNestSense(useRange) = newData; % overwrite
     end
 end
